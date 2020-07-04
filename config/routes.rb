710d7d0b575115d17c1daafe2ae88915f3831ae2
vagrant <vagrant@localhost.localdomain>
2020-07-04 07:32:09 +0000

    [finish]3-6

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,9 @@ Rails.application.routes.draw do
   get 'top' => 'homes#top'
 
   post 'todolists' => 'todolists#create'
+
+  get 'todolists' => 'todolists#index'
+
+  get 'todolists/:id' => 'todolists#show', as: 'todolist'
   
 end
